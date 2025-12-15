0c443caec099c8b5756f9465a3efce3ddc3c6f34
KevDev7 <96637803+KevDev7@users.noreply.github.com>
2025-12-14 23:29:23 -0600

    Delete 0_exploration/Untitled Notebook 2025-12-12 13:01:49.py

--- 0_exploration/Untitled Notebook 2025-12-12 13:01:49.py
+++
@@ -1,6 +0,0 @@
-# Databricks notebook source
-dbutils.fs.ls("/databricks-datasets/retail-org/")
-
-# COMMAND ----------
-
-spark.read.text("/databricks-datasets/retail-org/README.md").show(truncate=False)
